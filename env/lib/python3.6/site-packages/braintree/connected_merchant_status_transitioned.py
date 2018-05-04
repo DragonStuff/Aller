e85640ef439314737c3cf29517017c89bdfafbfe
DragonStuff <me@0xdragon.com>
2018-05-04 21:52:58 +1000

    Fixed untracked files.

--- env/lib/python3.6/site-packages/braintree/connected_merchant_status_transitioned.py
+++
@@ -1,6 +0,0 @@
-from braintree.resource import Resource
-
-class ConnectedMerchantStatusTransitioned(Resource):
-
-    def __init__(self, gateway, attributes):
-        Resource.__init__(self, gateway, attributes)
--- env/lib/python3.6/site-packages/daphne/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from hypothesis import HealthCheck, settings
-
-settings.register_profile(
-    'daphne',
-    settings(suppress_health_check=[HealthCheck.too_slow]),
-)
-settings.load_profile('daphne')
