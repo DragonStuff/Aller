f34811ea92d3ce1f45dd95a26aec7e363368f38a
DragonStuff <me@0xdragon.com>
2018-05-04 18:30:29 +1000

    Added new fields as per low-fi.

--- Aller/AllerNow/models.py
+++ Aller/AllerNow/models.py
@@ -1,23 +1,25 @@
-from django.urls import reverse
-from django_extensions.db.fields import AutoSlugField
 from django.conf import settings
+from django.contrib.auth import models as auth_models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth import get_user_model
-from django.contrib.auth import models as auth_models
 from django.db import models as models
+from django.db.models import *
+from django.urls import reverse
 from django_extensions.db import fields as extension_fields
+from django_extensions.db.fields import AutoSlugField
 
 
 class Location(models.Model):
 
     # Fields
-    name = models.CharField(max_length=255)
-    slug = extension_fields.AutoSlugField(populate_from='name', blank=True)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
-    converted_longitude = models.CharField(max_length=30)
-    converted_latitude = models.CharField(max_length=30)
+    name = CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', blank=True)
+    created = DateTimeField(auto_now_add=True, editable=False)
+    last_updated = DateTimeField(auto_now=True, editable=False)
+    converted_longitude = CharField(max_length=30)
+    converted_latitude = CharField(max_length=30)
 
 
     class Meta:
@@ -27,12 +29,43 @@ class Location(models.Model):
         return u'%s' % self.slug
 
     def get_absolute_url(self):
-        return reverse('AllerNow_location_detail', args=(self.slug,))
+        return reverse('app_name_location_detail', args=(self.slug,))
 
 
     def get_update_url(self):
-        return reverse('AllerNow_location_update', args=(self.slug,))
+        return reverse('app_name_location_update', args=(self.slug,))
 
+
+class Person(models.Model):
+
+    # Fields
+    slug = AutoSlugField(populate_from='name', blank=True)
+    created = DateTimeField(auto_now_add=True, editable=False)
+    last_updated = DateTimeField(auto_now=True, editable=False)
+    first_name = CharField(max_length=30)
+    last_name = CharField(max_length=30)
+    date_of_birth = DateField()
+    phone_number = models.CharField(max_length=10)
+    postcode = CharField(max_length=4)
+    email = models.CharField(max_length=30)
+    rating = models.PositiveSmallIntegerField()
+    address = models.TextField(max_length=100)
+
+    # Relationship Fields
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __unicode__(self):
+        return u'%s' % self.slug
+
+    def get_absolute_url(self):
+        return reverse('app_name_person_detail', args=(self.slug,))
+
+
+    def get_update_url(self):
+        return reverse('app_name_person_update', args=(self.slug,))
 
 class Car(models.Model):
 
@@ -47,22 +80,35 @@ class Car(models.Model):
         ('MO', 'Monthly'),
         ('YR', 'Yearly'),
     )
-    
+
     # Fields
-    name = models.CharField(max_length=255)
-    slug = extension_fields.AutoSlugField(populate_from='name', blank=True)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
-    body_type = models.CharField(max_length=100)
-    location = models.CharField(max_length=30)
-    price_per_unit = models.PositiveIntegerField()
-    unit_size = models.CharField(max_length=2, choices=TIMEUNIT_CHOICES, default=DAILY)
+    name = CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', blank=True)
+    created = DateTimeField(auto_now_add=True, editable=False)
+    last_updated = DateTimeField(auto_now=True, editable=False)
+    body_type = CharField(max_length=100)
+    price_per_unit = PositiveIntegerField()
+    unit_size = CharField(max_length=2, choices=TIMEUNIT_CHOICES, default=DAILY)
     year = models.CharField(max_length=4)
+    brand = models.CharField(max_length=30)
+    plate = models.CharField(max_length=8)
+    state = models.CharField(max_length=3)
+    registered_owner = models.CharField(max_length=30)
+    transmission = models.BooleanField()
+    condition = models.CharField(max_length=30)
+    kilometers = models.PositiveIntegerField()
+    fuel_type = models.CharField(max_length=30)
+    color = models.CharField(max_length=30)
+    seats = models.PositiveSmallIntegerField()
+    doors = models.PositiveSmallIntegerField()
+    available_from = models.DateField()
+    available_to = models.DateField()
+    image_url = models.URLField()
+    listing_type = models.CharField(max_length=2)
 
     # Relationship Fields
-    location = models.OneToOneField(
-        Location, null=True, blank=True, on_delete=models.CASCADE
-    )
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('-created',)
@@ -71,41 +117,8 @@ class Car(models.Model):
         return u'%s' % self.slug
 
     def get_absolute_url(self):
-        return reverse('AllerNow_car_detail', args=(self.slug,))
+        return reverse('app_name_car_detail', args=(self.slug,))
 
 
     def get_update_url(self):
-        return reverse('AllerNow_car_update', args=(self.slug,))
-
-
-class Person(models.Model):
-
-    # Fields
-    slug = extension_fields.AutoSlugField(populate_from='name', blank=True)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    date_of_birth = models.DateField()
-    phone_number = models.TextField(max_length=10)
-    postcode = models.CharField(max_length=4)
-
-    # Relationship Fields
-    cars = models.ForeignKey(
-        Car, null=True, blank=True, on_delete=models.CASCADE
-    )
-
-    class Meta:
-        ordering = ('-created',)
-
-    def __unicode__(self):
-        return u'%s' % self.slug
-
-    def get_absolute_url(self):
-        return reverse('AllerNow_person_detail', args=(self.slug,))
-
-
-    def get_update_url(self):
-        return reverse('AllerNow_person_update', args=(self.slug,))
-
-
+        return reverse('app_name_car_update', args=(self.slug,))
