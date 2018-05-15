962a79b6e29f91e5b91c45ba5406f7adb6cb2b85
DragonStuff <me@0xdragon.com>
2018-05-15 19:13:59 +1000

    Changes to settings for Localtunnel, working version of car detail template.

--- Aller/Aller/settings.py
+++ Aller/Aller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'CHANGEME')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
